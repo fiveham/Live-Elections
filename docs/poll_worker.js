acdd36996d12eb1ea663bc07e33a5a7bcb72d2e4
fiveham <fiveham@users.noreply.github.com>
2019-05-05 08:04:01 -0400

    Remove commented-out code

--- docs/poll_worker.js
+++ docs/poll_worker.js
@@ -9,9 +9,9 @@ var summary_url = null;
 var prev_results = {'updated': ""};
 var looperator = null;
 
-self.addEventListener('message', start2, false);
+self.addEventListener('message', start, false);
 
-function start2(event){
+function start(event){
   summary_url = event.data;
   cycle();
   looperator = setInterval(cycle, 60000);
@@ -43,54 +43,3 @@ function cycle(){
   
   //wait till next minute
 }
-
-/*function start(event){
-  var prev_results = {'updated': ""};
-  var summary_url = event.data;
-
-  var minute = (new Date()).getMinutes();
-  while(true){
-    var fetch = new XMLHttpRequest();
-    fetch.open("GET", summary_url+'?m='+minute, false);
-    
-    console.log("About to GET: "+minute);
-    fetch.send(null);
-    console.log("Just got: "+minute);
-    
-    var results = JSON.parse(fetch.responseText);
-    
-    if( results['updated'].valueOf() != prev_results['updated'].valueOf() ){
-      //send results to main thread as an "update" message
-      self.postMessage(results);
-    }
-    prev_results = results;
-    
-    if(results.isFinal){
-      break; //leave the loop
-    } else{
-      //wait until next minute
-      var test_minute;
-      while(true){
-        test_minute = (new Date()).getMinutes();
-        if(test_minute != minute){
-          minute = test_minute;
-          break;
-        } else{
-          wait_5();
-        }
-      }
-    }
-  }
-  //leave suicide note for main thread to find
-  self.postMessage({'an_hero': true});
-  //kill self
-  self.close();
-}
-
-async function wait_5() {
-  await sleep(5000);
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}/**/
